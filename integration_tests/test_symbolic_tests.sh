47d00d94160c5f4bde32c05252be7190b8d70bd1
Yohan <kmyohan0@gmail.com>
2020-02-27 19:27:47 -0800

    Fixed integration tests

--- integration_tests/test_symbolic_tests.sh
+++ integration_tests/test_symbolic_tests.sh
@@ -5,7 +5,7 @@
 [ -f bin ] >> test_symbolic_test_outputs
 [-e bin] >> test_symbolic_test_outputs
 ([ -e bin ]) >> test_symbolic_test_outputs
-([ -e bin] && echo hello) || echo notWorking  >> test_symbolic_test_outputs
-([ -f bin] && echo hello) || echo notWorking  >> test_symbolic_test_outputs
+([ -e bin ] && echo hello) || echo notWorking  >> test_symbolic_test_outputs
+([ -f bin ] && echo hello) || echo notWorking  >> test_symbolic_test_outputs
 ([ -e bin ] && echo hello) || (echo this && echo should_not_print) >> test_symbolic_test_outputs
 
