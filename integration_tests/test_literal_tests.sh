283d75e54df553f4941485f7f8094607e6882fec
Yohan <kmyohan0@gmail.com>
2020-02-28 09:42:52 -0800

    Still working on integration_tests

--- integration_tests/test_literal_tests.sh
+++ integration_tests/test_literal_tests.sh
@@ -3,6 +3,6 @@
 echo testing \"test\" literal integration tests
 
 echo
-../bin/rshell.out < "test_literal_test.txt"
+.././bin/rshell.out < "test_literal_test.txt"
 echo
 echo integration \"test\" literal \"test\" done
