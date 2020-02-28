371916356fa9ef1ec9f01d109e40cd5b2a1c5fb7
Yohan <kmyohan0@gmail.com>
2020-02-28 13:57:27 -0800

    Done fixing integration tests

--- integration_tests/multiple_commands_tests.sh
+++ integration_tests/multiple_commands_tests.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-echo hello; echo world  >> multiple_command_outputs
-ls -a; echo world >> multiple_command_outputs
-echo "hello"; ls >> multiple_command_outputs
-echo hello; echo whatsup; echo ryan >> multiple_command_outputs
-ls; ls -a >> multiple_command_outputs
-echo "hello world"; echo again; >> multiple_command_outputs
-ls -a; ls; echo FINALLY;  >> multiple_command_outputs
-echo A && echo B >> connector_commands_output
-echo this || echo notthis >> connector_commands_output
-ls && ls -a >> connector_commands_output
-echo A && echo B && echo C >> connector_commands_output
-echo A || echo B || echo C >> connector_commands_output
-echo correct || echo sodontoutputthis >> connector_commands_output
-echa error || echo this should print >> connector_commands_output
-exit >> connector_commands_output
+.././rshell < multiple_command_test
--- integration_tests/single_command_tests.sh
+++ integration_tests/single_command_tests.sh
@@ -1,20 +1,3 @@
+#!/bin/sh
 
-#!/bin/bash
-
-
-echo hello >> single_command_outputs
-ls >> single_command_outputs
-echo "hello" >> single_command_outputs
-ls -a >> single_command_outputs
-echo A >> single_command_outputs
-echo "hello world" >> single_command_outputs
-"echo" hello >> single_command_outputs
-ls >> single_command_outputs
-echo "hello" >> quotation_command_outputs
-"echo hello" >> quotation_command_outputs
-echo " hello " >> quotation_command_outputs
-echo "" >> quotation_command_outputs
-ec"ho hello" >> quotation_command_outputs
-"ls -a" >> quotation_command_outputs
-exit >> quotation_command_outputs
-
+.././rshell < single_command_test
