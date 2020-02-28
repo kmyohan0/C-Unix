09f26bb325b6c54e882cc6b8e02ff7fc809717c0
Yohan <kmyohan0@gmail.com>
2020-02-27 19:53:37 -0800

    Fixed integration tests

--- integration_tests/test_literal_tests.sh
+++ integration_tests/test_literal_tests.sh
@@ -1,6 +1,6 @@
 echo testing \"test\" literal integration tests
 
 echo
-./../bin/rshell.out < "test_literal_tests"
+../bin/rshell.out < "test_literal_tests"
 echo
 echo integration \"test\" literal \"test\" done
